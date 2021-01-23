61a28da5dbfa14c04da9853656dd4386b74204cd
gemcloud <gemcloud85@gmail.com>
2021-01-23 12:09:01 -0800

    Add project files.

---
+++ AdminLTEWebDashboard/Controllers/JwtTokenController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdminLTEWebDashboard.Controllers
+{
+    public class JwtTokenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateJwt()
+        {
+            return View();
+        }
+
+        public IActionResult RefreshToken()
+        {
+            return View();
+        }
+    }
+}
